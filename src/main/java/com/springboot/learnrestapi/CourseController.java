74750aa1ee40d7274d7d95a11e5f134f2a84bcbe
Debnath <2144048@cognizant.com>
2023-05-15 11:07:46 +0530

    /courses endpoint added

--- src/main/java/com/springboot/learnrestapi/CourseController.java
+++ src/main/java/com/springboot/learnrestapi/CourseController.java
@@ -3,10 +3,13 @@ package com.springboot.learnrestapi;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
+	
+	@RequestMapping("/courses")
 	public List<Course> retrieveAllCourses(){
 		return Arrays.asList(
 				new Course(1,"Learn Rest API", "with Satyajit"),
